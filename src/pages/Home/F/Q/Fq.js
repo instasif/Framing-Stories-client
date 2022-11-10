6fbe213735f80722809e5062faa65d6bbdcde016
instasif <asifh9277@gmail.com>
2022-11-10 20:41:07 +0600

    added questions card

--- src/pages/Home/F/Q/Fq.js
+++ src/pages/Home/F/Q/Fq.js
@@ -5,8 +5,37 @@ const Fq = () => {
         <div className="text-center mb-4">
             <p className='text-2xl font-bold text-orange-600 mt-10'>Question/Answer</p>
             <h2 className='text-5xl font-semibold'>Get Started</h2>
-            <div></div>
-            
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mx-auto my-auto'>
+
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title"> s</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            </div>
+
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            </div>
+
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            </div>
+
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+            </div>
+
+            </div>
         </div>
     );
 };
