26a6da4dc2576c027f6d3aca8fe1a6925b408326
instasif <asifh9277@gmail.com>
2022-11-10 22:10:57 +0600

    added environment variable

--- src/Firebase/Firebase.cofig.js
+++ src/Firebase/Firebase.cofig.js
@@ -5,19 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_apiKey,
-    // authDomain: process.env.REACT_APP_authDomain,
-    // projectId: process.env.REACT_APP_projectId,
-    // storageBucket: process.env.REACT_APP_storageBucket,
-    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    // appId: process.env.REACT_APP_appId,
-
-    apiKey: "AIzaSyDqm-UywGor2Hw-diFCHB-nDcd2TPkSsuk",
-    authDomain: "framing-stories.firebaseapp.com",
-    projectId: "framing-stories",
-    storageBucket: "framing-stories.appspot.com",
-    messagingSenderId: "822866072147",
-    appId: "1:822866072147:web:75aa561330c5e6cd72aca6"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
